096b52cab37811b79227bcc21f2c1db7bec1f78c
f.lecuyer <f.lecuyer@criteo.com>
2026-03-31 17:07:07 +0200

    💣 Remove old config
    Change-Id: Ia7b7df1d425c53eb771a1fab1981a49da1c19bd7

--- lua/flecuyer/init.lua
+++
@@ -1,2 +0,0 @@
-require("flecuyer.set")
-require("flecuyer.keymap")
--- lua/plugins/ollama.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "David-Kunz/gen.nvim",
-    keys = {
-        { "<leader>oo", "<cmd>Gen<cr>", mode= {"n", "v"} },
-    },
-    config = function()
-      require("gen").model = "codellama"
-    end
-}
